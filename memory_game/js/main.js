ae4e3d24302847aca2908fa3eaa83a04bbc373ba
Benckendorf Zac <BenckendorfZac@JohnDeere.com>
2020-07-29 08:04:42 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,13 +1,19 @@
 const cards = ['queen','queen','king','king'];
 const cardsInPlay = [];
-let cardOne = cards[0];
-cardsInPlay.push('cardOne');
-console.log("User flipped " + cardOne);
-let cardTwo = cards[2];
-cardsInPlay.push('cardTwo');
-console.log("User flipped " + cardTwo);
-if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[2]) {
-	alert("You found a match");	
+
+function checkForMatch() {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+  console.log("You found a match!");
 } else {
-	alert("Sorry try again");
+  console.log("Sorry, try again.");
 }
+}
+function flipCard(cardId) {
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	if (cardsInPlay.length === 2) {
+		checkForMatch();
+}	
+}
+flipCard([0]);
+flipCard([2]);
