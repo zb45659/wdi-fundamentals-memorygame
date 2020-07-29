7d4748ab110134ee17a5d74898bd439da50a8577
Benckendorf Zac <BenckendorfZac@JohnDeere.com>
2020-07-29 10:54:45 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -29,14 +29,24 @@ function checkForMatch() {
   alert("Sorry, try again.");
 }
 }
-function flipCard(cardId) {
+function flipCard() {
+	let cardId = this.getAttribute('data-id');
 	console.log("User flipped " + cards[cardId].rank);
 	cardsInPlay.push(cards[cardId].rank);
-	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
+	console.log(cards[cardId].cardImage);
+	this.setAttribute('src' , cards[cardId].cardImage);
 	if (cardsInPlay.length === 2) {
 		checkForMatch();
 }	
 }
-flipCard([0]);
-flipCard([2]);
+function createBoard(){
+	for (let i = 0; i < cards.length; i++) {
+		let cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+}
+}
+createBoard();
