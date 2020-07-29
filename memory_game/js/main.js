cdc912cec3b24b4cc17c52ae8f04541472a6916e
Benckendorf Zac <BenckendorfZac@JohnDeere.com>
2020-07-28 20:59:44 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,13 @@
-console.log("Up and running");
-let cardOne = "queen"
-let cardTwo = "queen"
-let cardThree = "king"
-let cardFour = "king"
+const cards = ['queen','queen','king','king'];
+const cardsInPlay = [];
+let cardOne = cards[0];
+cardsInPlay.push('cardOne');
 console.log("User flipped " + cardOne);
-console.log("User flipped " + cardFour);
+let cardTwo = cards[2];
+cardsInPlay.push('cardTwo');
+console.log("User flipped " + cardTwo);
+if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[2]) {
+	alert("You found a match");	
+} else {
+	alert("Sorry try again");
+}
